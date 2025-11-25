1de631852238213595e9050b6ca62e6bed7e653e
Rohan V <147333642+rohanv2007@users.noreply.github.com>
2025-11-25 19:40:03 +0530

    Update page.jsx

--- rohan-vault/app/page.jsx
+++ rohan-vault/app/page.jsx
@@ -3,7 +3,7 @@
 
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import { supabase } from "../../lib/supabaseClient";
+import { supabase } from "../lib/supabaseClient";
 import "./home.css";
 import Toast from "./components/Toast";
 
